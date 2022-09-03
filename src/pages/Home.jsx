d2253d2df49bd478ab0c47ff53289f9b5f9cf7af
RITIK KUMAR <ritikkr70@gmail.com>
2022-09-03 17:07:30 +0530

    First Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Announcement from '../components/Announcement'
-import Navbar from '../components/Navbar'
-import Slider from '../components/Slider'
-
-
+import React from "react";
+import Announcement from "../components/Announcement";
+import Categories from "../components/Categories";
+import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
-      <Announcement/>
-      <Navbar/>
-      <Slider/>
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Categories/>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
