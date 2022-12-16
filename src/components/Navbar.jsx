a0fe2d1acf42c7c89d22c88582590bbecd81c02e
ritikkr <ritikkr70@gmail.com>
2022-12-16 19:22:58 +0530

    dec update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>LAMA.</Logo>
+          <Logo>SHOPEE.</Logo>
         </Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
